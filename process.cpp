89799dfc82e6f8091412f1aeb2ea72574d89b42b
kwekwe789 <kwkyeifor@gmail.com>
2025-05-12 13:35:43 -0400

    Update and rename process.cpp to Shell_Program/process.cpp

--- process.cpp
+++
@@ -1,4 +0,0 @@
-#include "process.h"
-
-Process::Process(): id_(0), arrival_(0), burst_(0), wt(0), rt(0), tt(0){}
-Process::Process(int id, int arrival, int burst): id_(id), arrival_(arrival), burst_(burst), wt(0), rt(0), tt(0){}
